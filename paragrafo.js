703032def8dcbd1b57b12ecb3ef8c4a19c2159a5
Lucas Rodrigues Cunha <123827882+LucasRodriguesCunha@users.noreply.github.com>
2023-08-08 14:54:32 -0300

    I added paragrafo.js in the head

---
+++ paragrafo.js
@@ -0,0 +1,10 @@
+const paragrafos = [
+    "Embora ninguém possa voltar atrás e fazer um novo começo, qualquer um pode começar agora e fazer um novo fim.",
+
+    "A vida é uma peça de teatro que não permite ensaios. Por isso, cante, chore, dance, ria e viva intensamente, antes que a cortina se feche e a peça termine sem aplausos.",
+    "Às vezes só ouvimos falar de ódio, mas há mais amor neste mundo do que você poderia imaginar.",
+
+    "Nunca deixe ninguém te dizer que não pode fazer alguma coisa. Se você tem um sonho tem que correr atrás dele. As pessoas não conseguem vencer e dizem que você também não vai vencer. Se você quer uma coisa corre atrás.",
+    
+    "Motivação é a arte de fazer as pessoas fazerem o que você quer que elas façam porque elas o querem fazer.",
+];
